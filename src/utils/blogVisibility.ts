2569307cc1fe6000d51b3c013434488cf6932a7a
nlyu1 <lyuxingjian@gmail.com>
2026-02-28 10:40:43 -0500

    Hide OT parts 0-3 in production builds

---
+++ src/utils/blogVisibility.ts
@@ -0,0 +1,12 @@
+const PROD_HIDDEN_BLOG_SLUGS = new Set([
+  'machine-learning/ot-generative-0-static',
+  'machine-learning/ot-generative-1-wasserstein-geometry',
+  'machine-learning/ot-generative-2-drifting-models',
+  'machine-learning/ot-generative-3-maximum-likelihood-smooth-drifting'
+]);
+
+export const isBlogHiddenInProduction = (slug: string): boolean =>
+  import.meta.env.PROD && PROD_HIDDEN_BLOG_SLUGS.has(slug);
+
+export const filterVisibleBlogs = <T extends { slug: string }>(entries: T[]): T[] =>
+  entries.filter((entry) => !isBlogHiddenInProduction(entry.slug));
